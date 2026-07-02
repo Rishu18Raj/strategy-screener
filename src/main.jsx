c2d5948b9ee15599d55c5eccadef29cf2584c980
Rishu18Raj <37923176+Rishu18Raj@users.noreply.github.com>
2026-07-02 09:21:40 +0530

    Add posthog

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import posthog from 'posthog-js'
+
+   posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+     api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+   })
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
